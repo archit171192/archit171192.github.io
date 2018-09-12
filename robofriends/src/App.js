53dac6f593f32b7f0c2a10abd9bd6ff2faa5f4cf
Archit Gupta <archit.gupta@booking.com>
2018-09-13 00:24:34 +0200

    added Scoll component

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox'
+import Scroll from './Scroll';
 import './App.css';
 
 
@@ -29,7 +30,9 @@ class App extends Component {
                 <div className='tc'>
                     <h1 className='f1'>Robofriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
-                    <CardList robots={ filteredRobots }/>
+                    <Scroll>
+                        <CardList robots={ filteredRobots }/>
+                    </Scroll>
                 </div>
             );
         }
